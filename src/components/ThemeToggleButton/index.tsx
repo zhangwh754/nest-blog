55034e43b006ad0e724e5bd3f9e2910e206810c7
张文皓 <zhangwh754@163.com>
2023-11-23 16:21:57 +0800

    remove useless style
    - 移除ThemeToggleButton无用的focus类

--- src/components/ThemeToggleButton/index.tsx
+++ src/components/ThemeToggleButton/index.tsx
@@ -40,7 +40,7 @@ export default function ThemeToggleButton() {
     <div>
       <p>current theme：{theme}</p>
       <button
-        className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+        className="bg-slate-900 hover:bg-slate-700 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
         onClick={() => toggleTheme()}
       >
         toggleTheme
