aec3165d041c056ce61b18d3d4b836f325a951fd
张文皓 <zhangwh754@163.com>
2023-11-29 13:38:34 +0800

    移动端导航栏2
    - 添加移动端导航栏
    - 导航栏样式微调
    - 防止移动端导航栏打开时滚动穿透

--- src/app/(root)/Header/index.tsx
+++ src/app/(root)/Header/index.tsx
@@ -9,6 +9,16 @@ import ThemeToggleButton from './components/ThemeToggleButton'
 export default function Header() {
   const [display, setDisplay] = useState(false)
 
+  const toggleMenuShow = (display: Boolean) => {
+    if (display) {
+      document.documentElement.classList.add('overflow-hidden')
+      setDisplay(true)
+    } else {
+      document.documentElement.classList.remove('overflow-hidden')
+      setDisplay(false)
+    }
+  }
+
   return (
     <>
       <header>
@@ -20,17 +30,17 @@ export default function Header() {
             <div className="hidden h-6 text-2xl font-semibold sm:block">zwh</div>
           </div>
 
-          <nav className="hidden ml-auto text-base font-medium leading-5 sm:block">
-            <Link className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4" href="/">
+          <nav className="hidden ml-auto text-base font-medium sm:block">
+            <Link className="p-1 text-gray-900 dark:text-gray-100 sm:p-4" href="/">
               Blog
             </Link>
-            <Link className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4" href="/tag">
+            <Link className="p-1 text-gray-900 dark:text-gray-100 sm:p-4" href="/tag">
               Tags
             </Link>
-            <Link className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4" href="/chat">
+            <Link className="p-1 text-gray-900 dark:text-gray-100 sm:p-4" href="/chat">
               Chat
             </Link>
-            <Link className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4" href="/about">
+            <Link className="p-1 text-gray-900 dark:text-gray-100 sm:p-4" href="/about">
               About
             </Link>
           </nav>
@@ -38,14 +48,28 @@ export default function Header() {
           {display && (
             <div className="fixed top-0 left-0 z-10 h-full w-full transform bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-gray-800 translate-x-0">
               <div className="py-10 px-4 text-white flex justify-end">
-                <MenuToggleButton display={false} toggleMenuShow={() => setDisplay(false)} />
+                <MenuToggleButton display={false} toggleMenuShow={() => toggleMenuShow(false)} />
               </div>
+              <nav className="flex flex-col items-start px-8 text-2xl font-bold">
+                <Link className="p-4 text-gray-900 dark:text-gray-100" href="/">
+                  Blog
+                </Link>
+                <Link className="p-4 text-gray-900 dark:text-gray-100" href="/tag">
+                  Tags
+                </Link>
+                <Link className="p-4 text-gray-900 dark:text-gray-100" href="/chat">
+                  Chat
+                </Link>
+                <Link className="p-4 text-gray-900 dark:text-gray-100" href="/about">
+                  About
+                </Link>
+              </nav>
             </div>
           )}
 
           <ThemeToggleButton />
 
-          <MenuToggleButton display={true} toggleMenuShow={() => setDisplay(true)} />
+          <MenuToggleButton display={true} toggleMenuShow={() => toggleMenuShow(true)} />
         </div>
       </header>
     </>
